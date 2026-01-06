0e26aa7e233aebce10d59ead0c90fdc6f3d21202
aishwarrya2006 <aishwarrya.t2006@gmail.com>
2026-01-07 00:06:15 +0530

    Added

--- Easy/_13_TransformArrayByParity.java
+++ Easy/_13_TransformArrayByParity.java
@@ -2,17 +2,28 @@ class Solution {
     public int[] transformArray(int[] nums) 
     {
         int n = nums.length;
+        int count0 = 0;
         int ans[] = new int[n];
         for(int i = 0; i<n; i++)
         {
             if(nums[i]%2 == 0)
             {
-                ans[i] = 0;
+                nums[i] = 0;
+                count0++;
             }
             else
             {
-                ans[i] = 1;
+                nums[i] = 1;
             }
+        }
+
+        for(int j=0; j<count0; j++)
+        {
+            ans[j] = 0;
+        }
+        for(int k=count0; k<n; k++)
+        {
+            ans[k] = 1;
         }
         return ans;
     }
